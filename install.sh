94141877e47ffa87f3caee8e470156d577f377c0
ahwaz14 <119062798+ahwazi12@users.noreply.github.com>
2024-10-21 21:59:21 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -60,7 +60,9 @@ fi
 cd knp
 echo "Building Docker image for knp_backend..."
 docker build -t knp_backend .
-docker run -it -v /root/knp/.env:/knp_backend/.env -v /root/knp/backup_config.json:/knp_backend/backup_config.json -v /root/knp/main.conf:/knp_backend/main.conf --entrypoint "node" knp_backend config.js
+docker run -it -v /root/knp/.env:/knp_backend/.env -v /root/knp/backup_config.json
+:/knp_backend/backup_config.json -v /root/knp/main.conf:/knp_backend/main.conf --e
+ntrypoint "node" knp_backend config.js
 DOMAINS=$(docker run -it -v /root/knp/.env:/knp_backend/.env -v /root/knp/backup_c
 onfig.json:/knp_backend/backup_config.json --entrypoint "node" knp_backend get_cer
 t_urls.js)
